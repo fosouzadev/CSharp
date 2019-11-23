a08bb33fe0841386b4a8f169ce2001d2dcf9701f
Felipe Oliveira de Souza <fo.souza@outlook.com>
2019-11-23 10:11:03 -0300

    Base Conversion

---
+++ CSharp/CSharp.Test/ConversionsTest/BaseConversionTest.cs
@@ -0,0 +1,52 @@
+﻿using Conversions;
+using Xunit;
+
+namespace CSharp.Test.ConversionsTest
+{
+    public class BaseConversionTest
+    {
+        [Theory]
+        [InlineData(11, "1011")]
+        [InlineData(78, "1001110")]
+        [InlineData(96, "1100000")]
+        public void DecimalToBinary_Default_ReturnBinary(long number, string expected)
+        {
+            string actual = BaseConversion.DecimalToBinary(number);
+
+            Assert.Equal(actual, expected);
+        }
+
+        [Theory]
+        [InlineData(11, "B")]
+        [InlineData(78, "4E")]
+        [InlineData(96, "60")]
+        public void DecimalToHexadecimal_Default_ReturnHexadecimal(long number, string expected)
+        {
+            string actual = BaseConversion.DecimalToHexadecimal(number);
+
+            Assert.Equal(actual, expected);
+        }
+
+        [Theory]
+        [InlineData("1011", 11)]
+        [InlineData("1001110", 78)]
+        [InlineData("1100000", 96)]
+        public void BinaryToDecimal_Default_ReturnDecimal(string value, long expected)
+        {
+            long actual = BaseConversion.BinaryToDecimal(value);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("B", 11)]
+        [InlineData("4E", 78)]
+        [InlineData("60", 96)]
+        public void HexadecimalToDecimal_Default_ReturnDecimal(string value, long expected)
+        {
+            long actual = BaseConversion.HexadecimalToDecimal(value);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CSharp/Conversions/BaseConversion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Conversions
+{
+    public static class BaseConversion
+    {
+        public static string DecimalToBinary(long number)
+        {
+            return Convert.ToString(number, toBase: 2);
+        }
+
+        public static string DecimalToHexadecimal(long number)
+        {
+            return Convert.ToString(number, toBase: 16).ToUpper();
+        }
+
+        public static long BinaryToDecimal(string binary)
+        {
+            return Convert.ToInt64(binary, 2);
+        }
+
+        public static long HexadecimalToDecimal(string binary)
+        {
+            return Convert.ToInt64(binary, 16);
+        }
+    }
+}
